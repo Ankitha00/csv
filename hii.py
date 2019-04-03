204b184f057ffea2ca9cd6c768a6123d4a9ea3bb
Ankitha00 <48045236+Ankitha00@users.noreply.github.com>
2019-04-03 11:29:57 +0530

    Add files via upload

---
+++ hii.py
@@ -0,0 +1,13 @@
+a=int(input("enter 1st number"))
+b=int(input("enter 2nd number"))
+
+def sum():
+    if a==b:
+        print("a=b \n bye")
+        return 0
+        
+    else:
+        print(a+b)
+
+sum()
+    
